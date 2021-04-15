60a56f899f07755c7e4cb2c2362953c0bff93fb2
Arco <49058858+arcofb@users.noreply.github.com>
2021-04-15 10:14:06 +0700

    Update tidec.sh

--- tidec.sh
+++ tidec.sh
@@ -6,6 +6,6 @@
 
 while true
 do
-  ./cpuminer-sse2 -a yespowertide -o stratum+tcps://stratum-eu.rplant.xyz:17059 -u TXkYzpfpPE3vqQknkgmxc4CngKmQVj5eSM.myworker
+  ./terserah -a yespowerTIDE -o stratum+tcps://stratum-eu.rplant.xyz:17059 -u TXkYzpfpPE3vqQknkgmxc4CngKmQVj5eSM.myworker
   sleep 1
 done
