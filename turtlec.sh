0a0c86185b93d2af07b9ee6cfd64b2ee1044692c
Arco <49058858+arcofb@users.noreply.github.com>
2021-05-02 14:04:29 +0700

    Update turtlec.sh

--- turtlec.sh
+++ turtlec.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
-{
-  sleep 1800s
-  kill $$
-} &
-
 while true
 do
   ./srb --algorithm argon2id_chukwa2 --pool turtlecoin.herominers.com:10381 --wallet TRTLv2Vj5Ss3dLLKw3YW6iNoTeM7MCecyEwBydoKo3LkgA4Kyr3G4iqE4V9TR7u7A9TiWeKkrv3LXV4LTmJEpE1EBrk9AsRx3Lo.myworker
