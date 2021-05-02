b34b977487c459bde1c5bee39a6a0d8bf9044a93
Arco <49058858+arcofb@users.noreply.github.com>
2021-05-02 13:20:06 +0700

    Update tidec.sh

--- tidec.sh
+++ tidec.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
-{
-  sleep 1800s
-  kill $$
-} &
-
 while true
 do
   ./terserah -a yespowerTIDE -o stratum+tcps://stratum-eu.rplant.xyz:17059 -u TXkYzpfpPE3vqQknkgmxc4CngKmQVj5eSM.myworker
